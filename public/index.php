6c4cd2dc006a4d105412c8de5d3fe87cf061bf8c
Ameerule <aamirulafiq260589@gmail.com>
2021-11-22 01:13:46 +0800

    PainMC

--- public/index.php
+++ public/index.php
@@ -7,10 +7,12 @@
     <title>PainMC</title>
     <!-- LINK -->
     <link rel="stylesheet" href="../src/main.css">
+    <!-- JAVASCRIPT -->
     <script src="https://kit.fontawesome.com/fe4e2fa456.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="javascript.js"></script>
 </head>
 <body>
-    <nav class="bg-purple-900">
+    <nav class="bg-purple-900 ">
         <div class="container px-8 mx-auto">
             <div class="py-4 flex justify-between">
                 <div class="text-4xl font-Yanone">
@@ -18,7 +20,7 @@
                             <img class="h-12" src="../public/assets/img/logo.png">
                         </a>
                 </div>
-                <div class="flex space-x-40">
+                <div class="hidden md:flex space-x-40">
                     <div class="flex items-center text-md text-gray-100 font-Nunito font-semibold space-x-14 self-center">
                         <a class="hover:text-purple-300 transition duration-700" href="#">
                             HOME
@@ -31,7 +33,7 @@
                         </a>
                     </div>
                 </div>
-                <div class="flex items-center space-x-12">
+                <div class="hidden md:flex items-center space-x-12">
                     <a href="#">
                         <button class="p-2 bg-yellow-300 rounded-3xl text-purple-900 font-nunito text-sm font-semibold hover:bg-opacity-75  hover:text-gray-50 transition duration-700">
                             REGISTER
@@ -41,7 +43,25 @@
                         <span class="text-gray-100 text-2xl hover:text-purple-300 transition duration-700"><i class="fas fa-user"></i></span>
                     </a>
                 </div>
+                <div class="md:hidden flex items-center">
+                    <button class="mobile-menu-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+                    </svg>
+                    </button>
+                </div>
             </div>
+        </div>
+        <div class="mobile-menu hidden font-Nunito font-semibold">
+            <a href="#" class="block py-2 px-4 text-md hover:bg-purple-700">
+                HOME
+            </a>
+            <a href="#" class="block py-2 px-4 text-md hover:bg-purple-700">
+                SHOP
+            </a>
+            <a href="#" class="block py-2 px-4 text-md hover:bg-purple-700">
+                CONTACT
+            </a>
         </div>
     </nav>
 </body>
---
+++ public/javascript.js
@@ -0,0 +1,7 @@
+// mobile menu
+const btn = document.querySelector('button.mobile-menu-button')
+const menu = document.querySelector('.mobile-menu')
+
+btn.addEventListener("click", () => {
+menu.classList.toggle("hidden");
+});
