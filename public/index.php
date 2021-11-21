3cc7094a3f22620d18e26ce747083c6aa3bb3cb0
Ameerule <aamirulafiq260589@gmail.com>
2021-11-22 00:14:49 +0800

    PainMC

---
+++ public/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PainMC</title>
+    <!-- LINK -->
+    <link rel="stylesheet" href="../src/main.css">
+    <script src="https://kit.fontawesome.com/fe4e2fa456.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="bg-purple-900">
+        <div class="container px-8 mx-auto">
+            <div class="py-4 flex justify-between">
+                <div class="flex space-x-40">
+                    <div class="text-4xl font-Yanone">
+                        <a href="#" class="flex items-center">
+                            <img class="h-12" src="../public/assets/img/logo.png">
+                        </a>
+                    </div>
+                    <div class="flex items-center text-md text-gray-100 font-Nunito font-semibold space-x-14 self-center">
+                        <a class="hover:text-purple-300 transition duration-700" href="#">
+                            HOME
+                        </a>
+                        <a class="hover:text-purple-300 transition duration-700" href="#">
+                            SHOP
+                        </a>
+                        <a class="hover:text-purple-300 transition duration-700" href="#">
+                            CONTACT
+                        </a>
+                    </div>
+                </div>
+                <div class="flex items-center space-x-12">
+                    <a href="#">
+                        <button class="p-2 bg-yellow-300 rounded-3xl text-purple-900 font-nunito text-sm font-semibold hover:text-gray-50 transition duration-700">
+                            REGISTER
+                        </button>
+                    </a>
+                    <a href="#">
+                        <span class="text-gray-100 text-2xl hover:text-purple-300 transition duration-700"><i class="fas fa-user"></i></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'Nunito': 'sans-serif',
+        'Yanone': 'sans-serif'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
